aa281afa045109bf4ab3fc3a5f341965b35e76c1
AstridDahl <88832493+AstridDahl@users.noreply.github.com>
2022-09-15 16:03:54 +0200

    merge_vers1

--- src/merge.py
+++ src/merge.py
@@ -16,7 +16,43 @@ def merge(x: list[int], y: list[int]) -> list[int]:
     # FIXME: fill out the loop so you merge the lists
     # until one of them is empty
     while i < len(x) and j < len(y):
-        break  # FIXME: you shouldn't just break here
+        
+        if x[i] < y[j]:
+            z.append(x[i])
+            i += 1
+        else:
+            z.append(y[j])
+            j += 1
+        
+    #    break  # FIXME: you shouldn't just break here
     # At least one of the lists is empty now. Copy the
     # remainder of the other into z.
+
+        # if i == len(x):
+        #     for j in range(j, len(y)):
+        #         z.append(y[j])
+
+        # if j == len(y) and i != len(x):
+        #     for i in range(i, len(x)):
+        #         z.append(x[i])
+        
+        if i == len(x):
+            z.extend(y[j:len(y)])
+
+        if j == len(y) and i != len(x):
+            z.extend(x[i:len(x)])
+            # z.extend() adds the index content of one list to the list z, 
+            # whereas z.append() adds a list to the list z, so it becomes nested z=[i,i,i[i,i]]
+
     return z
+
+# Terminates because the while loop condition must become false as we increment i/j for each step.
+
+x=[1,2,3,7,7,6]
+y=[1,2,3]
+
+merge_list=merge(x,y)
+
+print(x,y)
+print(merge(x,y))
+print(f'The merged list: {merge_list}')
